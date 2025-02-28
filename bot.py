4d97bdbb8d35aed966801344fcb83d83bd61ba57
Alexandra M Dudzinski <alexandra.dudzinski@gmail.com>
2025-02-28 17:18:44 +0100

    Working on bot.py - Connection issues need fixing

--- bot.py
+++ bot.py
@@ -1,12 +1,35 @@
 import time
 import numpy as np
 import pandas as pd
+from ib_insync import *
 
-def main():
-    print("Starting CrocoQuant trading bot...")
-    while True:
-        print("Fetching market data...")
-        time.sleep(5)  # Simulating a trading loop
+ib=IB()
+ib.connect('127.0.0.1', 7497, 1) #Connection does not work
+stock = Stock('AAPL', 'SMART', 'USD')
 
-if __name__ == "__main__":
-    main()
+
+bars = ib.reqHistoricalData(
+    stock,
+    endDateTime='',
+    durationStr='1 D',
+    barSizeSetting='5 mins',
+    whatToShow='MIDPOINT',
+    useRTH=True
+)
+
+df = util.df(bars)
+print(df)
+
+ib.disconnect()
+
+
+
+#def main():
+#    print("Starting CrocoQuant trading bot...")
+#    while True:
+#        print("Fetching market data...")
+#
+#        time.sleep(5)  # Simulating a trading loop
+#
+#if __name__ == "__main__":
+#    main()
